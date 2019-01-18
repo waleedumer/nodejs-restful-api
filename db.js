df5b2f73dd3f6c09d75dffae619b5b21720c4263
waleedumer <waleedumer42@outlook.com>
2019-01-18 14:03:17 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb+srv://waleedumer_dev:WiDiiii4@free-aws-cluster-74bpk.mongodb.net/friesFactory?retryWrites=true');
